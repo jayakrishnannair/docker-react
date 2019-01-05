6a738d2d90df12cdaa5d582fb5bc9d4d43e9ab37
jayakrishnan.b.nair@gmail.com <Gauri2012>
2019-01-05 13:35:14 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <p className="App-intro">
+        Hi There
+        </p>
       </div>
     );
   }
